89cb08bfe31425aa1a88f0576fee1fc8aa451d1d
DANISH ZULFIQAR <rana.noman79773@gmail.com>
2026-06-20 09:03:50 +0500

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,6 @@ if st.sidebar.button("🗑️ Clear Chat"):
 @st.cache_resource
 def load_vectorstore():
 
-```
 embedding = HuggingFaceEmbeddings(
     model_name="sentence-transformers/all-MiniLM-L6-v2"
 )
